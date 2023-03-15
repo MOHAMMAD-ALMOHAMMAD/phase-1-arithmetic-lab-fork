89a509c2c13ec5efd56288d182a508fcee41ee55
MOHAMMAD ALMOHAMMAD <neotaiki123@gmail.com>
2023-03-15 19:48:13 +0300

    Completed the Assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+// start of first task
+const num1=31;
+const num2=2
+const multiply=num1*num2;
+//end of first task
+
+//start of second task
+const random=Math.ceil(Math.random());
+//end of second task
+
+
+//start of third task
+
+const num3=4
+const num4=11
+const mod=num3 % num4;
+//end of third task
+
+//start of fourth task
+
+const max=Math.max(1,4,0,20);
+//end of fourth task
+
+//output the results
+console.log(multiply,random,mod,max);
